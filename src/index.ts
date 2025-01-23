069d4079b54227ecbeb938ac421d7c4b4edde928
Ross Ragsdale <rossragsdale@gmail.com>
2025-01-23 02:40:25 -0600

    :construction: WIP

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@ export * from "./core/sdk";
 export * from "./core/router";
 export * from "./core/vault";
 export * from "./core/opcode";
-export * from "./core/mexc-vault";
 export * from "./types";
--- src/utils/mexc.ts
+++ src/utils/mexc.ts
@@ -1,9 +1,10 @@
-import { Spot } from "@theothergothamdev/mexc-sdk";
+// import { Spot } from "@theothergothamdev/mexc-sdk";
 import "dotenv/config";
 
 const apiKey = process.env.MEXC_API_KEY;
 const apiSecret = process.env.MEXC_API_SECRET;
 
-const client = new Spot(apiKey, apiSecret);
+// const client = new Spot(apiKey, apiSecret);
+const client = {} as any;
 
 export { client };
